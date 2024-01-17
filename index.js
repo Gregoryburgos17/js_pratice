7a48252a66da5e6941a9bd9716505647c726e70c
Gregory Burgos <gregoryburgos17@gmail.com>
2024-01-17 13:36:10 -0400

    [upp]: practica

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let firtname ="gregory";
 let lastname="BUrgos";
+
 console.log(`hello ${firtname} ${lastname}` );
---
+++ strings.js
@@ -0,0 +1,10 @@
+// creating single-line strings
+let firtName="Gregory";
+let lastName="Burgos";
+let title =`VP Engineering`
+title= `CTO`
+
+//String concatenation with the plus operator
+let fullName = firtName+ ""+ lastName;
+fullName=`${firtName} ${lastName}`
+console.log(fullName)
