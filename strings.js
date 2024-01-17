87f0600095e71f05a419dff7f5902ff8b201bdd8
Gregory Burgos <gregoryburgos17@gmail.com>
2024-01-17 16:55:52 -0400

    [upp] : string changer

--- strings.js
+++ strings.js
@@ -10,5 +10,22 @@ fullName=`${firtName} ${lastName}`
 console.log(fullName)
 
 // Creating multi-line strings with \n
-let bio ="line 1\nline 2\nLine 3\nLine "
+let bio =`About gregory Burgos
+gregory burgos is the current CTO of globomantic.
+he also serves as an author for pluralsight`;
 console.log(bio)
+
+//escaping characters
+let quote ='gregory said "JavaScript is great."';
+quote="gregory said, \"JavaScript is great.\"";
+quote=`javascript can use 'single' and "double" quotes.`; 
+quote=`In javascript, you must escape the \\ character`;
+console.log(quote);
+
+//String length
+let length =quote.length;
+console.log(`Quote Length: ${length}`);
+
+//Accessing specific characters
+let secondCharacter= quote[1];
+console.log(`Second Character : ${secondCharacter}`)
