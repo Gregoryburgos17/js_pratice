cf720efafdcf6960964b76bddc66803fa826cea2
Gregory Burgos <gregoryburgos17@gmail.com>
2024-01-17 17:35:34 -0400

    [add ] booleans file

---
+++ booleans.js
@@ -0,0 +1,11 @@
+// definig booleans
+let trueValue =true;
+console.log(trueValue);
+let falseValue= false;
+console.log(falseValue);
+
+// Using the NOT operator with Booleans
+let notTrue= !true;
+console.log(notTrue)
+let NotFalse =!false;
+console.log(NotFalse)
