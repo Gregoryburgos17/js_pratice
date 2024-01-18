da654915cb70c23383b7207603843017f1699af0
Gregory Burgos <gregoryburgos17@gmail.com>
2024-01-18 13:39:21 -0400

    [add] : add numbers

---
+++ numbers.js
@@ -0,0 +1,13 @@
+// Define number values
+let num1 =6;
+let num2 =-1000000;
+let num3 =1.234567;
+
+//Definig big and small numbers
+
+let num4 =1560000000000000000000;
+console.log(num4);
+let num5 =-0.0000000000000000021;
+console.log(num5);
+
+//creating
